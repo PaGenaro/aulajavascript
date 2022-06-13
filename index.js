f91e06c407b577092dfe51a14ed2a632eb76b007
PaGenaro <80373174+PaGenaro@users.noreply.github.com>
2022-06-13 01:24:00 -0300

    "Exercicio"

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let idade = 19
+if (idade > 18 )
+console.log('Você é maior de Idade')
+
+
+
+let maior=true
+if(maior > 18)
+console.log('Sim você é de maior')
+console.log(maior)
+
+
+let aniversario = 'Junho'
+
+if (aniversario == 1 || aniversario == 12){
+    console.log ('Feliz Aniversário!')
+}else{
+    console.log('Seu aniversário não é hoje!')
+}
